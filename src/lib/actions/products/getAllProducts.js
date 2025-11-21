17a8588322c7c6f0eca63bb4afe39e82e5afb939
Al Farishi <62209112+ralfarishi@users.noreply.github.com>
2025-11-21 20:23:39 +0700

    refactor(actions): implement on server sorting

--- src/lib/actions/products/getAllProducts.js
+++ src/lib/actions/products/getAllProducts.js
@@ -4,26 +4,27 @@ import { cache } from "react";
 import { createClient } from "@/lib/actions/supabase/server";
 
 export const getAllProducts = cache(
-  async ({ page = 1, limit = 10, query = "", sortOrder = "asc" } = {}) => {
-    const supabase = await createClient();
+	async ({ page = 1, limit = 10, query = "", sortOrder = "asc", sortBy = "name" } = {}) => {
+		const supabase = await createClient();
 
-    const from = (page - 1) * limit;
-    const to = from + limit - 1;
+		const from = (page - 1) * limit;
+		const to = from + limit - 1;
 
-    let dbQuery = supabase
-      .from("Product")
-      .select("id, name, description, createdAt", { count: "exact" })
-      .order("name", { ascending: sortOrder === "asc" })
-      .range(from, to);
+		let dbQuery = supabase
+			.from("Product")
+			.select("id, name, description, createdAt", { count: "exact" })
+			.order(sortBy, { ascending: sortOrder === "asc" })
+			.range(from, to);
 
-    if (query) {
-      dbQuery = dbQuery.ilike("name", `%${query}%`);
-    }
+		if (query) {
+			dbQuery = dbQuery.ilike("name", `%${query}%`);
+		}
 
-    const { data, error, count } = await dbQuery;
+		const { data, error, count } = await dbQuery;
 
-    const totalPages = count ? Math.ceil(count / limit) : 0;
+		const totalPages = count ? Math.ceil(count / limit) : 0;
 
-    return { data, error, count, totalPages };
-  },
+		return { data, error, count, totalPages };
+	}
 );
+
--- src/lib/actions/size-price/getAll.js
+++ src/lib/actions/size-price/getAll.js
@@ -4,20 +4,21 @@ import { cache } from "react";
 import { createClient } from "@/lib/actions/supabase/server";
 
 export const getAllSizePrice = cache(
-  async ({ page = 1, limit = 10, sortOrder = "asc" } = {}) => {
-    const supabase = await createClient();
+	async ({ page = 1, limit = 10, sortOrder = "asc", sortBy = "size" } = {}) => {
+		const supabase = await createClient();
 
-    const from = (page - 1) * limit;
-    const to = from + limit - 1;
+		const from = (page - 1) * limit;
+		const to = from + limit - 1;
 
-    const { data, error, count } = await supabase
-      .from("ProductSizePrice")
-      .select("id, size, price, createdAt", { count: "exact" })
-      .order("size", { ascending: sortOrder === "asc" })
-      .range(from, to);
+		const { data, error, count } = await supabase
+			.from("ProductSizePrice")
+			.select("id, size, price, createdAt", { count: "exact" })
+			.order(sortBy, { ascending: sortOrder === "asc" })
+			.range(from, to);
 
-    const totalPages = count ? Math.ceil(count / limit) : 0;
+		const totalPages = count ? Math.ceil(count / limit) : 0;
 
-    return { data, error, count, totalPages };
-  },
+		return { data, error, count, totalPages };
+	}
 );
+
